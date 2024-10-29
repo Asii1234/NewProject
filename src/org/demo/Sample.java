69302d41c20b69c068af53e48371220b5f16f8d7
Asii1234 <asikasii030@gmail.com>
2024-10-29 12:37:23 +0530

    done

--- src/org/demo/Sample.java
+++ src/org/demo/Sample.java
@@ -4,10 +4,15 @@ public class Sample {
 	private void sam() {
 System.out.println("Sample Project ");
 	}
+	
+	private void method3() {
+System.out.println("Suresh");
+	}
 public static void main(String[] args) {
 
 	Sample s = new Sample();
 	s.sam();
+	s.method3();
 	
 }
 }
